94be2d931eabf338fad3ed7752ae7b879fa181df
Farah Hossam <farah.alden2004@gmail.com>
2024-01-20 17:11:55 +0200

    test

--- test.java
+++ test.java
@@ -1,7 +1,14 @@
-class test {
+import java.util.Scanner;
 
+class test {
     public static void main(String arg[]){
 
         System.out.println("Test");
+
+        System.out.println("Kindly Enter your name :)");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+
+        System.out.println("Hello " + name + "!");
     } 
 }
